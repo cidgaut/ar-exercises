e1feb418e627d631cca765e2e63e5ab9298d8c1a
Cedric.gauthier <cidgaut@gmail.com>
2024-03-01 14:23:59 -0500

    created employees database

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Frank", last_name: "Sinatra", hourly_rate: 65)
+@store1.employees.create(first_name: "Kim", last_name: "Possible", hourly_rate: 55)
+@store1.employees.create(first_name: "Patty", last_name: "Crabby", hourly_rate: 200)
+
+@store2.employees.create(first_name: "Danny", last_name: "Phantom", hourly_rate: 100)
+@store2.employees.create(first_name: "Tiffany", last_name: "Enco", hourly_rate: 75)
+@store2.employees.create(first_name: "Igor", last_name: "Talern", hourly_rate: 175)
