845a7dc55cfdb8c23c0fe702215a90e5aa06ab26
Cedric.gauthier <cidgaut@gmail.com>
2024-03-01 14:10:20 -0500

    pulled data using filters. Used sum average and size as well as where

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total company revenue: $#{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue for all stores: $#{average_revenue}"
+
+high_revenue = Store.where('annual_revenue >= ?', 1000000).size
+puts "Number of stores with an annual earning of $1M or more: #{high_revenue}"
