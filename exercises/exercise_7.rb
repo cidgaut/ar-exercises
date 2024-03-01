75f6744b4d247be22117ab955489f4cf2268862d
Cedric.gauthier <cidgaut@gmail.com>
2024-03-01 14:34:31 -0500

    added validations to employee and store criteria. store creation via use input with error message handling based on criteria

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+new_store.save
+
+if new_store.errors.any?
+  puts "Failed to create the store due to the following errors:"
+  new_store.errors.full_messages.each do |error_message|
+    puts "- #{error_message}"
+  end
+else
+  puts "Store '#{store_name}' created successfully!"
+end
